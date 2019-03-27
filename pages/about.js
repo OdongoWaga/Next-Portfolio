ff77ab41ba4aae79c2f82b70896898f16a37ae3f
Waga Odongo <wagaodongo@gmail.com>
2019-03-27 15:28:50 +0300

    about refactor
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,17 @@
 import Link from "next/link";
 import Layout from "../components/Layout";
+import { Component } from "react";
 
-export default () => (
-	<Layout title="About">
-		<Link href="/">
-			<a> Go Home </a>
-		</Link>
-		<p> I'm a programmer </p>
-		<img src="/static/next-logo.png" alt=" next" height="200px" />
-	</Layout>
-);
+export default class About extends Component {
+	render() {
+		return (
+			<Layout title="About">
+				<Link href="/">
+					<a> Go Home </a>
+				</Link>
+				<p> I'm a programmer </p>
+				<img src="/static/next-logo.png" alt=" next" height="200px" />
+			</Layout>
+		);
+	}
+}
