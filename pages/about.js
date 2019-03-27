17785db9970deb286050bde381829a6ca4e2314f
Waga Odongo <wagaodongo@gmail.com>
2019-03-27 15:56:40 +0300

    fetching github data
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- pages/about.js
+++ pages/about.js
@@ -1,16 +1,35 @@
 import Link from "next/link";
 import Layout from "../components/Layout";
 import { Component } from "react";
+import fetch from "isomorphic-unfetch";
 
 export default class About extends Component {
+	static async getInitialProps() {
+		const res = await fetch("https://api.github.com/users/odongowaga");
+
+		const data = await res.json();
+
+		return { user: data };
+	}
+	// componentDidMount() {
+	// 	fetch("https://api.github.com/users/odongowaga")
+	// 		.then((res) => res.json())
+	// 		.then((data) => {
+	// 			this.setState({
+	// 				user: data
+	// 			});
+	// 		});
+	// }
+
 	render() {
+		const { user } = this.props;
+
 		return (
 			<Layout title="About">
-				<Link href="/">
-					<a> Go Home </a>
-				</Link>
+				<p> {user.name}</p>
+
 				<p> I'm a programmer </p>
-				<img src="/static/next-logo.png" alt=" next" height="200px" />
+				<img src={user.avatar_url} alt=" next" height="200px" />
 			</Layout>
 		);
 	}
